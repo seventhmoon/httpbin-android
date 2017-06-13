045558530cb9eb48e28ac65ba2f1234a2f047009
Fung <funglam@astri.org>
2017-06-13 15:43:48 +0800

    added 'post' in lib

--- app/src/main/java/com/androidfung/httpbin/ScrollingActivity.java
+++ app/src/main/java/com/androidfung/httpbin/ScrollingActivity.java
@@ -2,17 +2,15 @@ package com.androidfung.httpbin;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.androidfung.httpbin.http.HttpBinService;
 import com.androidfung.httpbin.http.ServicesManager;
-import com.androidfung.httpbin.http.model.GetResponse;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,31 +24,37 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                testService();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> testService());
     }
 
     private void testService(){
         HttpBinService httpBinService = ServicesManager.getHttpBinService();
 
-        httpBinService.get().enqueue(new Callback<GetResponse>() {
+        httpBinService.get().enqueue(new Callback<Map<String, Object>>() {
             @Override
-            public void onResponse(Call<GetResponse> call, Response<GetResponse> response) {
+            public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
                 Log.d(TAG, response.body().toString());
             }
 
             @Override
-            public void onFailure(Call<GetResponse> call, Throwable t) {
+            public void onFailure(Call<Map<String, Object>> call, Throwable t) {
+                Log.d(TAG, t.toString());
+            }
+
+        });
+
+        httpBinService.post().enqueue(new Callback<Map<String, Object>>() {
+            @Override
+            public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
+                Log.d(TAG, response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<Map<String, Object>> call, Throwable t) {
                 Log.d(TAG, t.toString());
             }
         });
--- library/src/main/java/com/androidfung/httpbin/http/HttpBinService.java
+++ library/src/main/java/com/androidfung/httpbin/http/HttpBinService.java
@@ -2,8 +2,11 @@ package com.androidfung.httpbin.http;
 
 import com.androidfung.httpbin.http.model.GetResponse;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by funglam on 6/13/17.
@@ -12,5 +15,8 @@ import retrofit2.http.GET;
 public interface HttpBinService {
 
     @GET("get")
-    Call<GetResponse> get();
+    Call<Map<String, Object>> get();
+
+    @POST("post")
+    Call<Map<String, Object>> post();
 }
